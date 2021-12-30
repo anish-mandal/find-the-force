e6cc0f297d695997eec569e806ff2e0760f10237
Anish <88893957+anish-mandal@users.noreply.github.com>
2021-12-30 14:58:02 +0530

    Fixed

--- force.py
+++ force.py
@@ -4,23 +4,19 @@ print("\nIn this program, we will find force applied on an object using python."
 # formula for Force
 print("Formula for force: F = ma (where F is force, m is mass and a is acceleration)\n")
 
-# True variable to stop the while loop
-true = True
 # While loop to keep checking for the value if entered an invalid value
-while(true is True):
+while(True):
     try:
         mass = int(input("Enter the mass of the object: "))
-        true = False
+        break
     except:
         print("You can only use mass as number.")
 
-# True variable to stop the while loop
-true_again = True
 # Again while loop to keep checking for the value if entered an invalid value
-while(true_again is True):
+while(True):
     try:
         acceleration = int(input("Enter the acceleration of the object: "))
-        true_again = False
+        break
     except:
         print("You can only use acceleration as number.")
 
