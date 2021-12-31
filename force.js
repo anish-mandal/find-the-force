14b40ff793cbbf9dd4733e21612acbf98e747bf6
Anish <88893957+anish-mandal@users.noreply.github.com>
2021-12-31 10:13:20 +0530

    Removed comments

--- force.js
+++ force.js
@@ -1,26 +1,16 @@
-// Program starts
-
-// Imports readline module to get user input
 const readline = require("readline");
 
-// Some instructions
 console.log("\nIn this program, we will find force applied on an object using python.");
 console.log("Formula for force: F = ma (where F is force, m is mass and a is acceleration)\n");
 
-// Input prompt starts
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-// First question
 rl.question("Enter the mass of the object: ", (mass) => {
-    // Second question
     rl.question("Enter the acceleration of the object: ", (acceleration) => {
         console.log(`The force applied on the object is ${mass*acceleration} Newton`)
         rl.close();
-        // Input prompt terminated
     });
 });
-
-// Program ends
