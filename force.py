16db3159620dc745af7e77438b03d12401616bad
Anish <88893957+anish-mandal@users.noreply.github.com>
2021-12-31 10:12:07 +0530

    Removed comments

--- force.py
+++ force.py
@@ -1,10 +1,6 @@
-# Program starts
-
 print("\nIn this program, we will find force applied on an object using python.")
-# formula for Force
 print("Formula for force: F = ma (where F is force, m is mass and a is acceleration)\n")
 
-# While loop to keep checking for the value if entered an invalid value
 while(True):
     try:
         mass = int(input("Enter the mass of the object: "))
@@ -12,15 +8,12 @@ while(True):
     except:
         print("You can only use mass as number.")
 
-# Again while loop to keep checking for the value if entered an invalid value
+        
 while(True):
     try:
         acceleration = int(input("Enter the acceleration of the object: "))
         break
     except:
         print("You can only use acceleration as number.")
-
-# Answer 
+ 
 print(f"The force applied on the object is {mass*acceleration} Newton")
-
-# Program ends
